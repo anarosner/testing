a29f802b360e4ce293466d5c25fd333756718d93
Ana Rosner <ana@alumni.tufts.edu>
2012-12-13 14:39:16 -0500

    Update rscripts/spatial_data.R

--- rscripts/spatial_data.R
+++ rscripts/spatial_data.R
@@ -19,7 +19,8 @@ library(maptools)
 
 # Base directory (change depending on file structure wherever this is being run)
 # basedir<-"C:/ALR/SpatialData"
-basedir<-"/home/ana/testing"
+basedir<-"/home/node.js/data/nhd"
+setwd(basedir)
 #basedir<-"C:/Documents/__USGS"
 
 ##### Define projections
@@ -33,15 +34,15 @@ proj4.NHD<-"+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs"
 
 ### load NHDPlus shapefiles and attribute dbf files
 #Flowlines
-setwd(paste0(basedir,"/NHDPlus/NHDPlusV21_NE_01_NHDSnapshot_01/NHDPlusNE/NHDPlus01/NHDSnapshot/Hydrography"))
+#setwd(paste0(basedir,"/NHDPlus/NHDPlusV21_NE_01_NHDSnapshot_01/NHDPlusNE/NHDPlus01/NHDSnapshot/Hydrography"))
 NEFlow.line<-readShapeLines("NHDFlowline",proj4string=CRS(proj4.NHD))
 
 #Catchments
-setwd(paste0(basedir,"/NHDplus/NHDPlusV21_NE_01_NHDPlusCatchment_01/NHDPlusNE/NHDPlus01/NHDPlusCatchment"))
+#setwd(paste0(basedir,"/NHDplus/NHDPlusV21_NE_01_NHDPlusCatchment_01/NHDPlusNE/NHDPlus01/NHDPlusCatchment"))
 NECatch.shape<-readShapePoly("Catchment",proj4string=CRS(proj4.NHD))
 
 #Attributes tables
-setwd(paste0(basedir,"/NHDplus/NHDPlusV21_NE_01_NHDPlusAttributes_01/NHDPlusNE/NHDPlus01/NHDPlusAttributes"))
+#setwd(paste0(basedir,"/NHDplus/NHDPlusV21_NE_01_NHDPlusAttributes_01/NHDPlusNE/NHDPlus01/NHDPlusAttributes"))
 plusflow<-read.dbf("PlusFlow.dbf")
 plusflowlineVAA<-read.dbf("PlusFlowlineVAA.dbf")
 
@@ -60,15 +61,15 @@ NHDCatch.shape<-NECatch.shape
 
    ### Create a subset of NHDplus flowlines/features
    ### lower CT R basin only, just to save time
-   lowCTFlow.line<-NEFlow.line[substr(NEFlow.line$REACHCODE,1,6)=="010802",] #HUC6
-   lowCTCatch.shape<-NECatch.shape[NECatch.shape$FEATUREID %in% lowCTFlow.line$COMID,]
+#   lowCTFlow.line<-NEFlow.line[substr(NEFlow.line$REACHCODE,1,6)=="010802",] #HUC6
+#   lowCTCatch.shape<-NECatch.shape[NECatch.shape$FEATUREID %in% lowCTFlow.line$COMID,]
 
    
    ### The NHDFlow.line and NHDCatch.shape will be used throughout the code
    ### Here, decide whether these should point to the full set of catchments/flowlines 
    ### for the New England region, or for a subsection of the Lower CT R basin
-   NHDFlow.line<-lowCTFlow.line
-   NHDCatch.shape<-lowCTCatch.shape
+#   NHDFlow.line<-lowCTFlow.line
+#   NHDCatch.shape<-lowCTCatch.shape
 
 
 
