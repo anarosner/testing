d09ead31c9734021b2e09e045cd22275499b408a
Ana Rosner <arosner@usgs.gov>
2013-01-21 00:53:34 -0500

    integrated changes to weather, flow, and s temp models

--- r/StreamFlowModel.R
+++ r/StreamFlowModel.R
@@ -5,46 +5,65 @@ source("./supporting_scripts/param_startup.R")
 
 
 #This code will need to recieve:
-#1) Directory for data I/O operations
-#2) Whether this is example (0) or other basin (1)
-#3) Set of monthly weather data
-#4) Basin Area in sq km
+# -monthly weather data
+# -Basin Area in sq km
 
-
-#Receive input from user, this should contain 1) working directory, 2) Example or User Basin, 3) Basin Area (km^2)
-# args <- commandArgs(TRUE)
-
-#Activate Libraries if needed
-#library(zoo)
-
-############How to Run Script from command line!########################################
-#as a default, run this script with the following arguments: '/home/austin/temp/ 0 52' 
 
 setwd(paste0(data_dir,"/flow_data/"))
 GCMDays <- read.table("Days_For_ABCDE_GCM.txt",header=TRUE)
+#need parms_web file, then activate this:
+# parms <- read.table("Parms_Web.txt",header=TRUE)
 
 #Read in weather generated from Climate Code
-#setwd(directory)
 setwd(preceding$climate_dir)
-
 stoc.weather <- read.table("monthly_weather.txt",header=TRUE)
 hist.weather <- read.table("h_monthly_weather.txt", header=TRUE)
 s_calib_num <- length(stoc.weather$MONTH)
 h_calib_num <- length(hist.weather$MONTH)
 
-#Westbrook Example
-if(run_option == 0){
-basin_area <- 10.81
-parameters <- c(0.934, 350.2514, 0.0000014, 0.62498, 0.4623, -0.7021, 9.9952) #SCE
-#parameters <- c(0.965,300,0.15,0.8,0.4,-0.5,15) #Bayes eyeball
-} else{
-#basin_area <- args[3]
-basin_param<-fromJSON(file=paste0(basin_dir,"/",basin_id,"/param.json"))
-basin_area<-basin_param$area
-###NEED TO FINISH
-###Parameter Selection Part Will Go Here
-parameters <- c(0.934, 350.2514, 0.0000014, 0.62498, 0.4623, -0.7021, 9.9952) #SCE
+#Read in basin paramters
+basin_id <- parse.param("basin_id")
+basin_param <- fromJSON(file=paste0(basin_dir,"/",basin_id,"/param.json"))
+basin_area <- basin_param$area
+basin_lat <- basin_param$lat
+basin_long <- basin_param$long
+
+
+#Get the fitted parameters for the basin
+{
+if(basin_id == "west_brook"){
+  #Westbrook Example
+    basin_area <- 10.81
+    parameters <- c(0.934, 350.2514, 0.0000014, 0.62498, 0.4623, -0.7021, 9.9952) #SCE
+    #parameters <- c(0.965,300,0.15,0.8,0.4,-0.5,15) #Bayes eyeball
+} 
+else {
+    
+# need parms_web file, then activate this:    
+#     a1 <- abs(parms$Lat)/57.29577951	#put everything in radians I think...
+#     b1 <- abs(parms$Long)/57.29577951
+#     a2<- abs(rep(lat,length(parms$Lat)))/57.29577951
+#     b2<- abs(rep(long,length(parms$Long)))/57.29577951
+#     h <- 6371*acos(round(sin(a1)*sin(a2)+cos(a1)*cos(a2)*cos(b1-b2),10)) #use law of cosines to compute distance 6371 km is radius of earth I think...
+#     
+#     #Find nearest Area-like gages -> use window of 30 kilometers, select smallest area gage, if none exist select nearest gage
+#     circle.radius <- 30 #in kilometers
+#     in.circle <- which(h < circle.radius)
+#     if (length(in.circle) == 0) {
+#       temp.parms <- parms[which(h == min(h)),]
+#       parameters <- c(temp.parms$A,temp.parms$B,temp.parms$C,temp.parms$D,temp.parms$E,temp.parms$F,temp.parms$DIF,temp.parms$Sint,temp.parms$Gint,temp.parms$Aint)
+#       snow <- temp.parms$ABCD
+#     } else {
+#       temp.parms <- parms[which(h < circle.radius),]
+#       DA.1 <- abs(temp.parms$SqMi*2.58999 - basin_area)
+#       temp.parms <- temp.parms[which(DA.1 == min(DA.1)),]
+#       parameters <- c(temp.parms$A,temp.parms$B,temp.parms$C,temp.parms$D,temp.parms$Sint,temp.parms$Gint)
+#       snow <- temp.parms$ABCD
+#     }
 }
+}
+
+# need parms_web file, replace this w/ code from Austin's script:    
 
 
 #Define state of ABCDE model
@@ -54,65 +73,65 @@ Aint <- 28.3
 
 ###Define ABCDE model as a Function########################################3
 ABCDE <- function(parameters,T,P,ET,Sint,Gint,Aint,calib_num) {
-A <- array(0,calib_num)
-S <- array(0,calib_num)
-G <- array(0,calib_num)
-y.hat <- array(0,calib_num)
-Y <- array(0,calib_num)
-W <- array(0,calib_num)
-E <- array(0,calib_num)
-mt <- array(0,calib_num)
-Peff <- array(0,calib_num)
-ETeff <- array(0,calib_num)
-SNOW_FRAC <- array(0,calib_num)
-
-a <- parameters[1]
-b <- parameters[2]
-c1 <- parameters[3]
-d <- parameters[4]
-e <- parameters[5]
-f <- parameters[6]
-dif <- parameters[7]
-
-A[1] <- Aint
-S[1] <- Sint
-G[1] <- Gint
-y.hat[1] <- 0
-Y[1] <- 0
-W[1] <- 0
-E[1] <- 0
-mt[1] <- 0
-Peff[1] <- P[1]
-ETeff[1] <- ET[1]
-
-for (i in 2:calib_num){
-#Snow Model
-SNOW_FRAC[i] <- (f - T[i])/(dif)
-if(T[i] >= (f-dif) && T[i] <= f){
-mt[i] <- (A[i-1] + P[i]*SNOW_FRAC[i])*e*(1-SNOW_FRAC[i])
-Peff[i] <- (P[i]*(1 - SNOW_FRAC[i]) + mt[i])
-ETeff[i] <- 0
-A[i] <- (A[i-1] + P[i]*SNOW_FRAC[i]) - mt[i]
-} else if(T[i] > f) {
-mt[i] <- A[i-1]
-Peff[i] <- P[i]+mt[i]
-ETeff[i] <- ET[i]
-A[i] <- A[i-1] - mt[i]
-} else {  #T[i] < (f-dif)
-mt[i] <- 0
-Peff[i] <- 0
-ETeff[i] <- 0
-A[i] <- A[i-1]+P[i]
-}
-#Begin ABCD Model
-W[i] <- Peff[i] + S[i-1]
-Y[i] <- (W[i]+b)/(2*a) - sqrt(((W[i]+b)/(2*a))^2-W[i]*b/a)
-S[i] <- Y[i]*exp(-1*ETeff[i]/b)
-E[i] <- Y[i]*(1-exp(-1*(ETeff[i]/b)))
-G[i] <- (G[i-1] + c1*(W[i]-Y[i]))/(1+d)
-y.hat[i] <- ((1-c1)*(W[i]-Y[i])+d*G[i])
-}
-return(y.hat)
+      A <- array(0,calib_num)
+      S <- array(0,calib_num)
+      G <- array(0,calib_num)
+      y.hat <- array(0,calib_num)
+      Y <- array(0,calib_num)
+      W <- array(0,calib_num)
+      E <- array(0,calib_num)
+      mt <- array(0,calib_num)
+      Peff <- array(0,calib_num)
+      ETeff <- array(0,calib_num)
+      SNOW_FRAC <- array(0,calib_num)
+      
+      a <- parameters[1]
+      b <- parameters[2]
+      c1 <- parameters[3]
+      d <- parameters[4]
+      e <- parameters[5]
+      f <- parameters[6]
+      dif <- parameters[7]
+      
+      A[1] <- Aint
+      S[1] <- Sint
+      G[1] <- Gint
+      y.hat[1] <- 0
+      Y[1] <- 0
+      W[1] <- 0
+      E[1] <- 0
+      mt[1] <- 0
+      Peff[1] <- P[1]
+      ETeff[1] <- ET[1]
+      
+      for (i in 2:calib_num){
+      #Snow Model
+      SNOW_FRAC[i] <- (f - T[i])/(dif)
+      if(T[i] >= (f-dif) && T[i] <= f){
+      mt[i] <- (A[i-1] + P[i]*SNOW_FRAC[i])*e*(1-SNOW_FRAC[i])
+      Peff[i] <- (P[i]*(1 - SNOW_FRAC[i]) + mt[i])
+      ETeff[i] <- 0
+      A[i] <- (A[i-1] + P[i]*SNOW_FRAC[i]) - mt[i]
+      } else if(T[i] > f) {
+      mt[i] <- A[i-1]
+      Peff[i] <- P[i]+mt[i]
+      ETeff[i] <- ET[i]
+      A[i] <- A[i-1] - mt[i]
+      } else {  #T[i] < (f-dif)
+      mt[i] <- 0
+      Peff[i] <- 0
+      ETeff[i] <- 0
+      A[i] <- A[i-1]+P[i]
+      }
+      #Begin ABCD Model
+      W[i] <- Peff[i] + S[i-1]
+      Y[i] <- (W[i]+b)/(2*a) - sqrt(((W[i]+b)/(2*a))^2-W[i]*b/a)
+      S[i] <- Y[i]*exp(-1*ETeff[i]/b)
+      E[i] <- Y[i]*(1-exp(-1*(ETeff[i]/b)))
+      G[i] <- (G[i-1] + c1*(W[i]-Y[i]))/(1+d)
+      y.hat[i] <- ((1-c1)*(W[i]-Y[i])+d*G[i])
+      }
+      return(y.hat)
 }
 
 
@@ -165,7 +184,7 @@ max.y <- max(mon.hist,max(mon.stoc)+15)
 
 #Make some graphics about Streamflow
 png(filename="StocStrFlow.png",width=725, height=575, bg="white")
-plot(seq(from=1,to=12,by=1),mon.hist[,2],col="black",type="b",pch=1,ylim=c(0,max.y),main="Streamflow",ylab="Streamflow (cfs)",xlab="Month")
-lines(seq(from=1,to=12,by=1),mon.stoc[,2],col="red")
-legend("topright", c("Stochastic/User Defined Climate", "Historic Climate"),col=c("red","black"),pch=c(-1,1),lwd=c(1,1),lty=c(1,1))
+  plot(seq(from=1,to=12,by=1),mon.hist[,2],col="black",type="b",pch=1,ylim=c(0,max.y),main="Streamflow",ylab="Streamflow (cfs)",xlab="Month")
+  lines(seq(from=1,to=12,by=1),mon.stoc[,2],col="red")
+  legend("topright", c("Stochastic/User Defined Climate", "Historic Climate"),col=c("red","black"),pch=c(-1,1),lwd=c(1,1),lty=c(1,1))
 dev.off()
--- r/supporting_scripts/param_startup.R
+++ r/supporting_scripts/param_startup.R
@@ -18,9 +18,12 @@ else {
 #load param functions
 source("./supporting_scripts/param_functions.R")
 
-
-directory<-args$run_dir
+this_dir <- getwd()
 run_dir<-args$run_dir
+  directory<-args$run_dir #to comply with old code
+basin_dir<-args$basin_dir
+data_dir<-args$data_dir
+
 setwd(run_dir)
 param<-fromJSON(file="param.json")
 
@@ -28,30 +31,30 @@ setwd(this_dir)
 config<-fromJSON(file=paste0(param$rscript,".json"))
 
 basin_id<-parse.param("basin_id")
-
-#Westbrook Example (0)
-# Any Basin (1)
-{
-	if (parse.param("basin_id")=="west_brook")
-	   run_option <-0
-	else
-	   run_option <-1
-}
+    
+    # #Westbrook Example (0)
+    # # Any Basin (1)
+    # {
+    # 	if (parse.param("basin_id")=="west_brook")
+    # 	   run_option <-0
+    # 	else
+    # 	   run_option <-1
+    # }
 
 preceding<-param$preceding
 
-# attach(param$preceding)
-# # 
-# #parse all the preceding run ids
-# if (!is.null(param$preceding)) {
-# #    pre<-param$preceding
-# #    length(pre)
-#    pre<-data.frame(dummy=c(1))
-#    for ( i in 1:length(param$preceding) ) {
-#       pre[i] <- param$preceding[i]     
-# #       preceding[paste0(names(pre[i]),"_dir")]<-paste0(basedir,"/runs/",ids$userid,"/",pre[i][[1]])      
-#    }
-#    preceding<-pre[,-1]
-# }
-
+    # attach(param$preceding)
+    # # 
+    # #parse all the preceding run ids
+    # if (!is.null(param$preceding)) {
+    # #    pre<-param$preceding
+    # #    length(pre)
+    #    pre<-data.frame(dummy=c(1))
+    #    for ( i in 1:length(param$preceding) ) {
+    #       pre[i] <- param$preceding[i]     
+    # #       preceding[paste0(names(pre[i]),"_dir")]<-paste0(basedir,"/runs/",ids$userid,"/",pre[i][[1]])      
+    #    }
+    #    preceding<-pre[,-1]
+    # }
+    
 
