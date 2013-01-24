1ce4959b4eaf67a1b48dfad62510cff5c5ce0a41
Ana Rosner <arosner@usgs.gov>
2013-01-24 12:50:40 -0500

    changed param.json to setting.json

--- r/supporting_scripts/param_startup.R
+++ r/supporting_scripts/param_startup.R
@@ -25,7 +25,7 @@ basin_dir<-args$basin_dir
 data_dir<-args$data_dir
 
 setwd(run_dir)
-param<-fromJSON(file="param.json")
+param<-fromJSON(file="setting.json")
 
 setwd(this_dir)
 config<-fromJSON(file=paste0(param$rscript,".json"))
