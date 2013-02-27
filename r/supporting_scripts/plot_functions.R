af97c1452228211b37bb89fd1e31290547ccb47c
Ana Rosner <arosner@usgs.gov>
2013-02-27 01:20:45 -0500

    fix incomplete par mai line

--- r/supporting_scripts/plot_functions.R
+++ r/supporting_scripts/plot_functions.R
@@ -73,7 +73,7 @@ plot.thumbnail<-function(type) {
       
       #write svg
       svg(filename="thumbnail.svg",width=2.5,height=2.5)
-      par(mai=)
+     # par(mai=)
           par(mai=c(.5,.2,.2,.4)) #svg (inches)  bottom, left, top and right
           #          par(mai=c(.75,.75,.1,.1)) #screen
           par(fg="white",col.main="slategray4",cex=p.s,cex.main=p.s)
